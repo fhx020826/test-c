a94072e836b332ff2760e871188860a743822f84
13244508819 <fhx020826@163.com>
2024-03-21 00:16:08 +0800

    3-20

---
+++ 2024-3-20/2024-3-20/test.c
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int get_bigger(int a, int b)
+//{
+//	return a > b ? a : b;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int ret = get_bigger(a, b);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//    int x = 0;
+//    int y = 0;
+//    scanf("%d", &x);
+//    if (x < 0)
+//    {
+//        y = 1;
+//    }
+//    else if (x == 0)
+//    {
+//        y = 0;
+//    }
+//    else
+//    {
+//        y = -1;
+//    }
+//    printf("%d", y);
+//    return 0;
+//}
+
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    scanf("%d %d", &a, &b);
+//    printf("%d %d", a / b, a % b);
+//    return 0;
+//}
+
+//int main()
+//{
+//    char arr[] = { 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116 , 33 };
+//    int i = 0;
+//    int sz = sizeof(arr) / sizeof(arr[0]);
+//    for (i = 0; i < sz; i++)
+//    {
+//        printf("%c", arr[i]);
+//    }
+//    return 0;
+//}
+
+//int main()
+//{
+//    int year = 0;
+//    int month = 0;
+//    int date = 0;
+//    scanf("%4d%2d%2d", &year, &month, &date);
+//    printf("year=%4d\nmonth=%02d\ndate=%02d", year, month, date);
+//    return 0;
+//}
+
+//int main()
+//{
+//    int id = 0;
+//    float c = 0.0f;
+//    float math = 0.0f;
+//    float english = 0.0f;
+//    scanf("%d;%f,%f,%f", &id, &c, &math, &english);
+//    printf("The each subject score of No. %d is %.2f, %.2f, %.2f.", id, c, math, english);
+//    return 0;
+//}
+
+//int main()
+//{
+//    int ret = printf("Hello world!");
+//    printf("\n%d\n", ret);
+//    return 0;
+//}
